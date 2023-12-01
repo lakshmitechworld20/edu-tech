5fa931c44fdfdebaa9498d72b2fee1b5ab07862c
dhoni9500 <kshiva8897@gmail.com>
2023-12-01 21:14:16 +0530

    add shivasai tasks

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import CoursesSlider from "./CoursesSlider";
+import AuthorsSlider from "./AuthorsSlider";
+
+function App() {
+  return (
+    <div className="App">
+      <AuthorsSlider />   
+      <CoursesSlider/>
+    </div>
+  );
+}
+
+export default App;
